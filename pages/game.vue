0d4ec62946b9c0933bc2bc29c128cfc9e86b334d
Vitaly Kezlya <vitaly@kezlya.com>
2018-12-19 12:37:16 +0300

    First draft of game page

---
+++ pages/game.vue
@@ -0,0 +1,45 @@
+<template>
+  <section>
+    <v-layout column align-center>
+      <v-flex xs12 md8 lg8>
+        <div id="player" class="ant-player">
+            <h2 class="loading">{{status}}</h2>
+        </div>
+      </v-flex>
+    </v-layout>
+  </section>
+</template>
+
+<script>
+//import { AnthivePlayer } from "anthive-player";
+
+export default {
+  data: () => ({
+    status: "Loading...",
+  }),
+  mounted() {
+    // let player_script = document.createElement('script');
+    //      player_script.setAttribute('src','/js/anthive.js')
+    //      document.head.appendChild(player_script);
+
+    console.log("sdfas",AnthivePlayer);
+
+   const gameid = this.$route.query.id || null;
+
+    const dataUrl = gameid ? "https://storage.googleapis.com/anthive-dev-games/" + gameid + ".zip" : null;
+    if (dataUrl != null){
+      const Myplayer  = new AnthivePlayer(dataUrl,"#player");
+    } else {
+      this.status = "Can't find game."
+    }
+    //Myplayer.play();
+    //console.log("sdfas",AnthivePlayer);
+  },
+}
+</script>
+<style>
+.ant-player {
+  background-image: url("https://anthive.io/skins/server/1/background.png");
+  background-repeat: repeat;
+}
+</style>
