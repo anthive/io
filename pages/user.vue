4fd85b820343f7e9c716b458713a35287159faee
Vitaly Kezlya <vitaly@kezlya.com>
2019-01-10 14:06:56 +0300

    Skeleton to call github API

---
+++ pages/user.vue
@@ -0,0 +1,58 @@
+<template>
+  <section>
+    <v-layout column align-center>
+      <v-flex xs12 md8 lg8>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <h1>Hello player </h1>
+
+      </v-flex>
+
+    </v-layout>
+  </section>
+</template>
+
+<script>
+//import { AnthivePlayer } from "anthive-player";
+import axios from "axios";
+
+
+export default {
+  data: () => ({
+    status: null
+  }),
+  mounted() {
+
+
+    const ghAxios = axios.create({
+      baseURL: 'https://api.github.com/',
+      timeout: 3000
+    });
+
+    const hhh = ghAxios.get("users/kezlya").then(function (data){
+      console.log(data);
+    });
+
+
+
+    // let player_script = document.createElement('script');
+    //      player_script.setAttribute('src','/js/anthive.js')
+    //      document.head.appendChild(player_script);
+
+
+  }
+}
+</script>
+<style>
+.ant-player {
+  background-image: url("https://anthive.io/skins/server/1/background.png");
+  background-repeat: repeat;
+  margin-top: 100px;
+}
+</style>
