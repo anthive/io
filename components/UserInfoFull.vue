da4a8e8b1a4634649d1daf475d9b8ca1646d02b9
AaZz Inc <zeniaroman@gmail.com>
2019-01-28 20:17:42 +0200

    Remove bot info from user chip

--- components/UserInfoFull.vue
+++ components/UserInfoFull.vue
@@ -23,27 +23,6 @@
           v-layout(row)
             v-card-text.py-2.font-weight-bold {{ username }}
               v-avatar.ml-2(size="10" :class="color")
-
-        //- v-flex.text-xs-left
-        //-   v-card-text.pt-1.pb-0.grey--text.text--darken-2.caption.text-xs-center Bot info
-        //-   v-card-text.py-0.caption
-        //-     span.user-info-full__meta-item-title Span:
-        //-     span Y{{ span[0] }}, X{{ span[1] }}
-        //-   v-card-text.py-0.caption
-        //-     span.user-info-full__meta-item-title Ticks:
-        //-     span {{ stats.Age }}
-        //-   v-card-text.py-0.caption
-        //-     span.user-info-full__meta-item-title Ants:
-        //-     span {{ stats.Ants }}
-        //-   v-card-text.py-0.caption
-        //-     span.user-info-full__meta-item-title HiveSize:
-        //-     span {{ stats.Hive }}
-        //-   v-card-text.py-0.caption
-        //-     span.user-info-full__meta-item-title Errors:
-        //-     span {{ stats.Errors }}
-        //-   v-card-text.py-0.caption
-        //-     span.user-info-full__meta-item-title Earned:
-        //-     span.font-weight-bold {{ score }}
 </template>
 
 <script>
