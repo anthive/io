d2f1261c3b1aeca1e663688aecd64571faf4e03b
staszigzag <stas.zigzag@gmail.com>
2019-10-15 18:12:35 +0300

    refactoring games page

--- components/UserInfo.vue
+++ components/UserInfo.vue
@@ -1,15 +1,16 @@
 <template lang="pug">
-	.user-info.grey.lighten-4.elevation-3.d-flex
-		v-img.user-info__avatar.d-inline-block.flex-grow-0(:src="us.photoUrl(username, 96)" width="48" height="48"  aspect-ratio="1" class="lighten-4")
-		v-img.user-info__avatar-lang(:src="us.langUrl(lang)"  width="48" height="48"  aspect-ratio="1"  class="lighten-2")
-		.text-xs-center.user-info__skin(:style="'background: green url('+us.hiveUrl(skin)+');'")
-			v-img.mx-auto(:src="us.antUrl(skin)" width="20")
-			span.black--text.text-center.d-block.user-info__version v.{{ version }}
-		.text-center.flex-grow-1.pt-3
-			a.user-info__link.black--text.font-weight-bold(:href="'/user/?username='+username") {{ username }}
-		.orange.lighten-5.pt-2
-			span.orange--text.title.font-weight-bold.mx-3 {{ score }}
-			v-icon.mr-3.orange--text.text--lighten-1(size="20") bubble_chart
+	.d-inline-block
+		.user-info.grey.lighten-4.elevation-3.d-flex
+			v-img.user-info__avatar.d-inline-block.flex-grow-0(:src="us.photoUrl(username, 96)" width="48" height="48"  aspect-ratio="1" class="lighten-4")
+			v-img.user-info__avatar-lang(:src="us.langUrl(lang)"  width="48" height="48"  aspect-ratio="1"  class="lighten-2")
+			.text-xs-center.user-info__skin(:style="'background: green url('+us.hiveUrl(skin)+');'")
+				v-img.mx-auto(:src="us.antUrl(skin)" width="20")
+				span.black--text.text-center.d-block.user-info__version v.{{ version }}
+			.text-center.flex-grow-1.pt-3.px-5
+				a.user-info__link.black--text.font-weight-bold(:href="'/user/?username='+username") {{ username }}
+			.orange.lighten-5.pt-2
+				span.orange--text.title.font-weight-bold.mx-3 {{ score }}
+					v-icon.orange--text.text--lighten-1(size="20") bubble_chart
 </template>
 
 <script>
@@ -37,6 +38,7 @@ export default {
 <style scoped lang="scss">
 	.user-info {
 		height: 48px;
+		max-width: 300px;
 		position: relative;
 		border-radius: 8px;
 		overflow: hidden;
--- pages/games.vue
+++ pages/games.vue
@@ -1,23 +1,15 @@
 <template lang="pug">
-  section(class="section section-lg pt-lg-0")
-    v-parallax(
-      src="/img/home_bg.png"
-      align
-      height="300"
-    )
-    v-layout(align-center justify-center row fill-height)
-      v-flex(xs12 md8 offset2)
-        v-card.games__card(
-          flat
-          tile
-          class="white text-xs-left pa-3 elevation-6 rules__card"
-        )
-          v-card-title
-            h1.headline
-              span.grey--text All battles:
-              span.ml-2 Explore, watch, replay, learn strategies
-          v-card-text
-            GamesTable(:Filters="filters" :PageSize=17 :ShowTitle="false")
+	section
+		v-parallax(src="/img/home_bg.png" height="300")
+		v-row(align="center" justify="center" )
+			v-col(cols="11" md="10")
+				v-card.games__card( flat tile class="white text-xs-left pa-3 elevation-6 ")
+					v-card-title.mb-10
+						h1.headline
+							span.grey--text All battles:
+							span.ml-2 Explore, watch, replay, learn strategies
+					v-card-text
+						GamesTable(:Filters="filters" :PageSize=17 :ShowTitle="false")
 </template>
 
 <script>
@@ -25,28 +17,28 @@ import userService from "@/services/User";
 import GamesTable from "@/components/gamesTable";
 
 export default {
-  name: 'defaultHeader',
-  data: () => ({
-    us: userService,
-    filters: [],
-    user: {}
-  }),
-  created() {
-    var username = 'kezlya'
+	name: 'defaultHeader',
+	data: () => ({
+		us: userService,
+		filters: [],
+		user: {}
+	}),
+	created() {
+		var username = 'kezlya'
 
-    this.filters = [
-      {"term": { "Age": 1000 }},
-      {"range": { "Wealth": { "gte": 1000, "lte": 3000 }}}
-    ];
-  },
-  components: {
-    GamesTable
-  }
+		this.filters = [
+			{"term": { "Age": 1000 }},
+			{"range": { "Wealth": { "gte": 1000, "lte": 3000 }}}
+		];
+	},
+	components: {
+		GamesTable
+	}
 }
 </script>
 
 <style>
-  .games__card {
-    margin-top: -178px;
-  }
+	.games__card {
+		margin-top: -178px;
+	}
 </style>
