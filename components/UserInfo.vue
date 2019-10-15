95eb6bdfd6743bc1f81d374a22288de522b145e7
staszigzag <stas.zigzag@gmail.com>
2019-10-15 17:19:26 +0300

    refactoring UserInfo

--- components/UserInfo.vue
+++ components/UserInfo.vue
@@ -1,107 +1,75 @@
 <template lang="pug">
-  .user-info.elevation-3
-    v-list.py-0
-      v-list-tile.grey.lighten-4
-        v-list-tile-content.user-info__photo
-          v-list-tile-avatar.user-info__photo-avatar.grey.lighten-4(size="48" tile)
-            v-img(:src="us.photoUrl(username, 96)" class="lighten-4")
-        v-list-tile-content.user-info__lang.grey.lighten-2
-          v-list-tile-avatar.user-info__lang-avatar(size="48" tile)
-            v-img(:src="us.langUrl(lang)" class="lighten-2")
-        v-list-tile-content.text-xs-center.user-info__skin(:style="'background: green url('+us.hiveUrl(skin)+');'")
-          img.mx-auto(:src="us.antUrl(skin)" width="20")
-          v-list-tile-sub-title.black--text.user-info__version v.{{ version }}
-        v-list-tile-content.px-3
-          v-list-tile-title.font-weight-bold
-            a.user-info__link(:href="'/user/?username='+username") {{ username }}
-        v-list-tile-content.px-3.orange.lighten-5
-          v-list-tile-sub-title.orange--text.title.font-weight-bold {{ score }}
-            v-avatar(size="20")
-              v-icon.ml-1.orange--text.text--lighten-1(size="20") bubble_chart
+	.user-info.grey.lighten-4.elevation-3.d-flex
+		v-img.user-info__avatar.d-inline-block.flex-grow-0(:src="us.photoUrl(username, 96)" width="48" height="48"  aspect-ratio="1" class="lighten-4")
+		v-img.user-info__avatar-lang(:src="us.langUrl(lang)"  width="48" height="48"  aspect-ratio="1"  class="lighten-2")
+		.text-xs-center.user-info__skin(:style="'background: green url('+us.hiveUrl(skin)+');'")
+			v-img.mx-auto(:src="us.antUrl(skin)" width="20")
+			span.black--text.text-center.d-block.user-info__version v.{{ version }}
+		.text-center.flex-grow-1.pt-3
+			a.user-info__link.black--text.font-weight-bold(:href="'/user/?username='+username") {{ username }}
+		.orange.lighten-5.pt-2
+			span.orange--text.title.font-weight-bold.mx-3 {{ score }}
+			v-icon.mr-3.orange--text.text--lighten-1(size="20") bubble_chart
 </template>
 
 <script>
 import userService from "@/services/User";
 
 export default {
-  name: "usesrInfo",
-  props: {
-    username: String,
-    lang: String,
-    version: String,
-    skin: Number,
-    score: Number
-  },
-  data: () => ({
-    us: userService
-  }),
-  created() {
-  },
-  methods: {
-  }
+	name: "usesrInfo",
+	props: {
+		username: String,
+		lang: String,
+		version: String,
+		skin: Number,
+		score: Number
+	},
+	data: () => ({
+		us: userService
+	}),
+	created() {
+	},
+	methods: {
+	}
 };
 </script>
 
-<style>
-  .user-info {
-    position: relative;
-    display: inline-block;
-    border-radius: 10px;
-    overflow: hidden;
-    cursor: default;
-    z-index: 3;
-    transition: all ease .5s;
-  }
+<style scoped lang="scss">
+	.user-info {
+		height: 48px;
+		position: relative;
+		border-radius: 8px;
+		overflow: hidden;
+		cursor: default;
+		z-index: 3;
 
-  .v-list__tile {
-    padding: 0;
-  }
+		&__avatar{
+			border-radius: 0;
+			z-index: 2;
 
-  .user-info__photo {
-    width: 48px;
-  }
-
-  .user-info__photo-avatar {
-    z-index: 2;
-  }
-
-  .user-info__photo:hover + .user-info__lang {
-    margin-left: 47px;
-  }
-
-  .user-info__photo:hover ~ .user-info__skin {
-    margin-left: 94px;
-  }
-
-  .user-info__lang {
-    position: absolute;
-    width: 48px;
-    z-index: 1;
-    transition: all ease .5s;
-  }
-
-  .user-info__lang-avatar {
-    z-index: 1;
-  }
-
-  .user-info__skin {
-    position: absolute;
-    width: 48px;
-    z-index: 1;
-    transition: all ease .5s;
-  }
-
-  .user-info__version {
-    position: relative;
-    bottom: -2px;
-    background: rgba(255, 255, 255, .8);
-  }
-
-  .user-info__link {
-    text-decoration: none;
-  }
-
-  .user-info__link:hover {
-    text-decoration: underline;
-  }
+			&:hover + .user-info__avatar-lang{
+				margin-left: 47px;
+			}
+			&:hover  ~ .user-info__skin{
+				margin-left: 94px;
+			}
+		}
+		&__avatar-lang{
+			position: absolute;
+			z-index: 1;
+			transition: all ease .5s;
+		}
+		&__skin {
+			position: absolute;
+			width: 48px;
+			z-index: 1;
+			transition: all ease .5s;
+		}
+		&__version {
+			background: rgba(255, 255, 255, .8);
+		}
+		&__link:hover {
+			text-decoration: underline;
+		}
+	}
 </style>
