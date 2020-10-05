8e4b5d47599b4e44535eadde10828c0234a8c45c
Vitaly Kezlya <vitaly@kezlya.com>
2020-10-05 14:57:59 +0300

    Sprint 14

--- _nuxt/04cf874.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{692:function(n,t,e){"use strict";e.r(t);var o={mounted:function(){this.$router.push("/sandbox/js")}},r=e(13),component=Object(r.a)(o,(function(){var n=this.$createElement;return(this._self._c||n)("div")}),[],!1,null,null,null);t.default=component.exports}}]);
