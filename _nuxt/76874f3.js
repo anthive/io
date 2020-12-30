296a79ac1715affbb485e21ccadaa389cd13245b
kezlya <kezlya@users.noreply.github.com>
2020-12-30 13:45:36 +0000

    [create-pull-request] automated change

--- _nuxt/76874f3.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[14],{1160:function(t,n,e){"use strict";e.r(n);var o={mounted:function(){this.$router.push("".concat(this.localePath("sandbox"),"/js"))}},c=e(7),component=Object(c.a)(o,(function(){var t=this.$createElement;return(this._self._c||t)("div")}),[],!1,null,null,null);n.default=component.exports}}]);
