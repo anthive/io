ff5a6bf9d22587e7d7a758b8c5efad357c0f982c
kezlya <kezlya@users.noreply.github.com>
2021-02-17 07:37:28 +0000

    [create-pull-request] automated change

---
+++ _nuxt/1b539d1.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{1179:function(t,e,n){"use strict";n.r(e);var o={mounted:function(){this.$router.push("".concat(this.localePath("leaderboard"),"/bots"))}},c=n(5),component=Object(c.a)(o,(function(){var t=this.$createElement;return(this._self._c||t)("div")}),[],!1,null,null,null);e.default=component.exports}}]);
